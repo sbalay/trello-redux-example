08b0b77f5e22b28b7ef2d682ea4a640a7a66c3de
Ignacio Abadie <ignacio.abadie@wolox.com.ar>
2017-09-11 10:06:15 -0300

    added documentation to array utils

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -1,6 +1,7 @@
 /*
 Function that receive a list and a index to remove a element list.
 Function find the element to remove by id, and then create a new list without the element.
+if cant find the element, function return original list
 Params:
   list: array of objects
   id: number
@@ -8,5 +9,7 @@ Params:
 
 export const removeArrayElementById = (list, id) => {
   const elementIndex = list.findIndex(element => element.id === id);
-  return list.slice(0, elementIndex).concat(list.slice(elementIndex + 1, list.length));
+  return elementIndex === -1
+    ? list
+    : list.slice(0, elementIndex).concat(list.slice(elementIndex + 1, list.length));
 };
